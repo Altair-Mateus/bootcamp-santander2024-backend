2a18d26cba3c8f5c4bed05d4184bf4d2bed9fd53
Altair-Mateus <taicoalencastro@gmail.com>
2024-07-25 22:42:37 -0300

    fix: Corrigdo projeto para usar o Graddle e não o maven conforme orientado pelo professor

--- Desafios/Publicando Sua API REST na Nuvem Usando Spring Boot 3, Java 17 e Railway/santander-bootcamp-2024/src/main/java/me/dio/SantanderBootcamp2024Application.java
+++ Desafios/Publicando Sua API REST na Nuvem Usando Spring Boot 3, Java 17 e Railway/santander-bootcamp-2024/src/main/java/me/dio/SantanderBootcamp2024Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  SantanderBootcamp2024Application {
+public class SantanderBootcamp2024Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SantanderBootcamp2024Application.class, args);
