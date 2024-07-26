6c922c8985ec40f4781b3057bb555526ea34eecd
Altair-Mateus <taicoalencastro@gmail.com>
2024-07-26 15:00:26 -0300

    feat: finalizado API

--- Desafios/Publicando Sua API REST na Nuvem Usando Spring Boot 3, Java 17 e Railway/bootcamp-santander-2024/src/main/java/me/dio/BootcampSantander2024Application.java
+++ Desafios/Publicando Sua API REST na Nuvem Usando Spring Boot 3, Java 17 e Railway/bootcamp-santander-2024/src/main/java/me/dio/BootcampSantander2024Application.java
@@ -1,8 +1,10 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Servr URL")})
 @SpringBootApplication
 public class BootcampSantander2024Application {
 
